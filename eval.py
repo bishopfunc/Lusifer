1910cc3cf79de229059375ed33bcf07b5ea9461e
bishopfunc <showyamamoto02@gmail.com>
2025-08-18 06:55:12 +0900

    update csv path

--- eval.py
+++ eval.py
@@ -3,6 +3,6 @@ import pandas as pd
 from movielens100K_example import evaluate_result
 
 if __name__ == "__main__":
-    rating_test_df = pd.read_csv("/Users/bishop/Lusifer/rating_test_df_test.csv")
+    rating_test_df = pd.read_csv("rating_test_df_test.csv")
     print(rating_test_df)
     evaluate_result(rating_test_df)
