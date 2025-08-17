1fb8bed0dc0c938d842ce085cf2c627a2c06daef
bishopfunc <showyamamoto02@gmail.com>
2025-08-18 06:27:29 +0900

    評価コード

---
+++ eval.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from movielens100K_example import evaluate_result
+
+if __name__ == "__main__":
+    rating_test_df = pd.read_csv("/Users/bishop/Lusifer/rating_test_df_test.csv")
+    print(rating_test_df)
+    evaluate_result(rating_test_df)
